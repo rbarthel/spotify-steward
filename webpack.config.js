6abae48a952808687a7755fd6ba76edfd02d5788
Russell Barthel <russellb@tbadigital.com>
2020-02-05 12:01:41 -0800

    setup react

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'client/dist')
-  }
-};
+    entry: './client/src/main.js',
+    output: {
+        path: path.join(__dirname, '/client/dist'),
+        filename: 'index_bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 8001
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: [
+                    path.resolve(__dirname, 'client/src')
+                ],
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        ]
+    },
+}
